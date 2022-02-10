5eba2c7b7ad54c43a97cf508a327dfc5ec525632
Xiaoming <1478599553@qq.com>
2022-02-10 18:24:54 +0800

    Update G_EntityPlayer.java

--- src/main/java/com/draming/groophite/api/G_EntityPlayer.java
+++ src/main/java/com/draming/groophite/api/G_EntityPlayer.java
@@ -3,6 +3,7 @@ package com.draming.groophite.api;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
+import scala.Int;
 
 public class G_EntityPlayer extends G_EntityLiving{
 
@@ -14,7 +15,7 @@ public class G_EntityPlayer extends G_EntityLiving{
     }
 
     public void setDead(){
-        this.player.attackEntityFrom(DamageSource.ANVIL);
+        this.player.attackEntityFrom(DamageSource.ANVIL, Integer.MAX_VALUE);
     }
     public void setHealth(float health){
         this.player.setHealth(health);
