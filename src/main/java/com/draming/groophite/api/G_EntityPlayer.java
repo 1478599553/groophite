9660cc3dbe5ac42f3acf32d2815f8bd934610cec
Xiaoming <1478599553@qq.com>
2022-02-10 20:31:28 +0800

    test

--- src/main/java/com/draming/groophite/api/G_EntityPlayer.java
+++ src/main/java/com/draming/groophite/api/G_EntityPlayer.java
@@ -15,13 +15,17 @@ public class G_EntityPlayer extends G_EntityLiving{
     }
 
     public void setDead(){
-        this.player.attackEntityFrom(DamageSource.ANVIL, Integer.MAX_VALUE);
+        this.player.attackEntityFrom(G_DamageSource.OUT_OF_WORLD.__innerDamageSource, Integer.MAX_VALUE);
     }
     public void setHealth(float health){
         this.player.setHealth(health);
     }
     public void setFire(int seconds){
         this.player.setFire(seconds);
+    }
+
+    public void attackEntityFrom(G_DamageSource g_damageSource, float amount){
+        this.player.attackEntityFrom(g_damageSource.__innerDamageSource,amount);
     }
 
     public G_Pos getPosition(){
