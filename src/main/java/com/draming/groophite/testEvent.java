c52bae31d82232998516e6b2298b0b86bbaa4939
Draming <1478599553@qq.com>
2022-03-05 20:02:04 +0800

    test
    test

--- src/main/java/com/draming/groophite/testEvent.java
+++ src/main/java/com/draming/groophite/testEvent.java
@@ -2,6 +2,7 @@ package com.draming.groophite;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.player.PlayerContainerEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -20,6 +21,25 @@ public abstract class testEvent {
     @SubscribeEvent
     public static void test(PlayerContainerEvent event) throws IOException {
 
+    }
+    @SubscribeEvent
+    public static void test2 (WorldEvent.CreateSpawnPosition event){
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        System.out.println("OK!!!!!!!!!!!!!!!Spawning!!!!");
+        event.getWorld().setWorldTime(13000);
     }
 }
 
