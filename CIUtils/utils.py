0d8b426c4714ed68395eb2b7a172309b655a7620
Draming <1478599553@qq.com>
2022-03-19 21:42:56 +0800

    t

--- CIUtils/utils.py
+++ CIUtils/utils.py
@@ -11,3 +11,23 @@ def getModVersion():
 if (sys.argv[1] == "postBuild"):
     mkdir("./artifacts/groophite/"+getModVersion())
     copyfile("./groophite-release.zip","./artifacts/groophite/"+getModVersion()+"/groophite_mc112_"+getModVersion()+".zip")
+
+tempWorkFlow = '''
+          
+          mkdir ./buildMod
+          cd ./buildMod
+          git clone https://github.com/1478599553/groophite.git
+          cd ./groophite
+          python ./buildMod/groophite/CIUtils/utils.py
+          ./gradlew.bat build
+
+          ./CIUtils/7z a ./build/groophite-release.zip ./build/libs/
+          Copy ./build/groophite-release.zip D:/groophite.zip
+          cd D:/buildMod
+          git clone https://github.com/1478599553/artifacts.git
+          python D:/buildMod/groophite/CIUtils/utils.py postBuild
+          cd D:/buildMod/artifacts
+          git add -A
+          git commit -m "autobuild"
+          git push origin main
+          '''
