1781205f1f5e3dceea69699216415bb8050ba97f
Draming <1478599553@qq.com>
2022-03-19 20:03:47 +0800

    stupid

--- CIUtils/utils.py
+++ CIUtils/utils.py
@@ -8,6 +8,6 @@ def getModVersion():
     src_file = open(r"./src/main/java/com/draming/groophite/groophite.java","r")
     src = src_file.read()
     print(src.split("String VERSION")[1].split(r'"',1)[1].split(r'"',1)[0])
-if (sys.argv[0] == "postBuild"):
+if (sys.argv[1] == "postBuild"):
     mkdir("~/buildMod/artifacts/groophite/"+getModVersion())
     copyfile("~/groophite.zip","~/buildMod/artifacts/groophite/"+getModVersion()+"/groophite_mc112_"+getModVersion()+".zip")
