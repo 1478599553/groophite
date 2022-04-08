e2320f914b13f7f5d0a989c9c0be4ef1450c1db2
Draming <64693215+1478599553@users.noreply.github.com>
2022-04-08 19:46:04 +0800

    Update groophite.java

--- src/main/java/com/draming/groophite/groophite.java
+++ src/main/java/com/draming/groophite/groophite.java
@@ -47,7 +47,7 @@ public class groophite
 {
     public static final String MODID = "groophite";
     public static final String NAME = "Groophite";
-    public static final String VERSION = "0.9.9999";
+    public static final String VERSION = "0.9.99999";
     @Mod.Instance
     public static groophite Instance;
 
